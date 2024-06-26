60b7e9262f5366891075e00f7582a37f137920e7
zpt <zpt@zpt.com.br>
2024-06-26 17:12:11 -0300

    feat(question6): BaseClass for connecting to db with other classes

--- src/User.php
+++ src/User.php
@@ -1,32 +1,15 @@
 <?php
 namespace User;
 
-class User {
-	private $db;
+use BaseClass;
 
+class User extends BaseClass{
 	public function find_many($ids) {
 		$users = [];
 
         $users[] = $this->db->q('SELECT username FROM user WHERE id IN (' . implode(',', $ids) . ')');
 
 		return $users;
-	}
-
-	public function setDb($db) {
-		if (!$db || $db->isClosed()) {
-			return false;
-		}
-
-		if ($db->debug) {
-			$db->setGeneralLog('on');
-			error_log($db);
-		}
-
-		if ($db->profiling) {
-			$db->setSlowLog('on');
-		}
-
-		$this->db = $db;
 	}
 }
 
