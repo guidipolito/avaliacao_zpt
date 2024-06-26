782b8c3c4c95bf4654f265f947125c314d3c39bf
Dipolito <guilhermepereiradipolito@hotmail.com>
2024-06-26 20:13:37 -0300

    feat(question4): implement largest department query

--- src/Department.php
+++ src/Department.php
@@ -4,10 +4,16 @@ namespace Department;
 require_once './User.php';
 
 class Department {
+    private $db;
+
 	private User\User $user;
 
 	public function __construct() {
 		$this->user = new User\User(); // @todo fixme
 	}
+
+    public static function largest(){
+        return $this->db->q('SELECT id, name FROM department ORDER BY employees DESC LIMIT 1')[0];
+    }
 }
 ?>
